c08a4da30b8684ac3bfff4609fc8251dc7759101
Austin McConnell <austin.s.mcconnell@gmail.com>
2017-09-07 20:49:54 -0500

    Converted to requirements.txt and requirements-to-freeze.txt files

--- setup.py
+++ setup.py
@@ -1,27 +1,16 @@
 from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
-    readme = readme_file.read()
+    README = readme_file.read()
 
-requirements = [
-    'Click >=6.7, <7.0',
-    'matplotlib >=1.5, <2.0',
-    'numpy >=1.11, <2.0',
-    'pandas >=0.19, <2.0',
-    'progressbar2 >=3.12, < 4.0',
-    'scikit-learn >=0.18, <1.0',
-    'seaborn >=0.7, <1.0'
-]
-
-test_requirements = [
-    'coverage >=4.3, <5.0'
-]
+with open('requirements.txt') as file:
+    REQUIREMENTS = file.read()
 
 setup(
     name='autolearn',
     version='0.1.0',
     description="Automatic Machine Learning",
-    long_description=readme,
+    long_description=README,
     author="Austin McConnell",
     author_email='austin.s.mcconnell@gmail.com',
     url='https://github.com/austinmcconnell/autolearn',
@@ -31,7 +20,7 @@ setup(
             'autolearn=autolearn.cli:main'
         ]
     },
-    install_requires=requirements,
+    install_requires=REQUIREMENTS,
     license="MIT license",
     zip_safe=False,
     keywords='autolearn',
@@ -45,6 +34,4 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    test_suite='tests',
-    tests_require=test_requirements
-)
+    test_suite='tests')
